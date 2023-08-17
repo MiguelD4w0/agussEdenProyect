312d31b1d45eb5b25c345a982d18fc895e20a551
MiguelD4w0 <miguel.lopez@darwoft.com>
2023-08-17 14:53:37 -0300

    Se agrego test de servicio

--- cypress/e2e/eden/edenServicios.cy.js
+++ cypress/e2e/eden/edenServicios.cy.js
@@ -2,9 +2,21 @@
 
 describe("TEST DE SERVICIO DE EDEN", ()=>{
 
-    it("Verificar Servicio de INICIO", () => {
-        cy.request("GET", "https://edenapi.edenentradas.com.ar/edenventarestapi2/api/contenido/inicio").then((respuesta) => {
+    it("Verificar Servicio de INICIO 1", () => {
+        cy.request("GET", "https://edenapi.edenentradas.com.ar/edenventarestapi2/api/contenido/inicio")
+        .then((respuesta) => {
+            cy.log("HOLA MUNDO")
+            cy.log(`Respuesta del servicio de inicio: ${JSON.stringify(respuesta)}`)
             expect(respuesta.status).to.eq(200);
+        });
+    });
+
+    it.only("Verificar Servicio de INICIO 2", () => {
+        cy.request({method: "GET", url:"https://edenapi.edenentradas.com.ar/edenventarestapi2/api/contenido/inicio"})
+        .then((respuesta) => {
+            cy.log(`Respuesta del servicio de inicio: ${JSON.stringify(respuesta)}`)
+            expect(respuesta.status).to.eq(200);
+            cy.writeFile("cypress/fixtures/autogenerado/eventos.json", respuesta["body"]);
         });
     });
 })
